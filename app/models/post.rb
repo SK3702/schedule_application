5f3ee8e5e3440781899058eae8e1a0393bb44732
SK3702 <seigokato618@gmail.com>
2024-09-11 20:47:43 +0900

    change/if文の中身が空白だってたをif not文へ変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,12 +7,9 @@ class Post < ApplicationRecord
   validate :start_end_check
 
   def start_end_check
-    if start_date.blank? || end_date.blank?
-    else
+    if not start_date.blank? || end_date.blank?
       errors.add(:end_date, "は開始日より前の日付は登録できません。") unless
       self.start_date < self.end_date
     end
-    
   end
-
 end
